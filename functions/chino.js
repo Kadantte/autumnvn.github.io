e4d03c4f7d71fcc5a8473b39c933ddbb924a8cf3
AutumnVN <htuan03@gmail.com>
2023-03-28 07:12:49 +0700

    882

--- functions/chino.js
+++ functions/chino.js
@@ -1,5 +1,5 @@
 export async function onRequest(context) {
-    const max = 881;
+    const max = 882;
     return new Response(await fetch("https://raw.githubusercontent.com/AutumnVN/chino.webp/main/" + Math.ceil(Math.random() * max) + ".webp").then((res) => res.blob()), {
         headers: {
             "content-type": "image/webp",
