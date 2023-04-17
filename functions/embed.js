c8fe177a006a1b2432faa38cbf80fbfa1b3b2eab
AutumnVN <htuan03@gmail.com>
2023-04-17 13:57:54 +0700

    embed

--- functions/embed.js
+++ functions/embed.js
@@ -147,7 +147,11 @@ export async function onRequest(context) {
         ${image && !banner && !video ? `<meta property="og:image" content="${image}" />` : ""}
         ${banner && !image && !video ? `<meta property="og:image" content="${banner}" />
         <meta name="twitter:card" content="summary_large_image" />` : ""}
-        ${video && !image && !banner ? `<meta property="og:video:url" content="${video}" />` : ""}
+        ${video && !image && !banner ? `<meta property="og:video" content="${video}" />
+        <meta property="og:video:type" content="video/mp4" />
+        <meta property="og:video:width" content="1280" />
+        <meta property="og:video:height" content="720" />
+        ` : ""}
         ${color ? `<meta name="theme-color" content="${color}" />` : ""}
         <meta property="og:url" content="${_url}" />
 
